137158c70a6fcbe073fe6b10cf61176cc312929c
Miles di Prata <milesdiprata3@gmail.com>
2026-01-08 09:27:47 -0800

    Day 5 part 2

--- src/day5.rs
+++ src/day5.rs
@@ -13,9 +13,17 @@ struct Range {
     len: u32,
 }
 
+#[derive(Clone, Debug)]
+#[cfg_attr(test, derive(PartialEq, Eq))]
+struct SeedRange {
+    start: u32,
+    len: u32,
+}
+
 #[derive(Debug)]
 struct Almanac {
     seeds: Vec<u32>,
+    seed_ranges: Vec<SeedRange>,
     seed_to_soil: Vec<Range>,
     soil_to_fertilizer: Vec<Range>,
     fertilizer_to_water: Vec<Range>,
@@ -68,6 +76,14 @@ impl FromStr for Almanac {
             .map(str::parse)
             .collect::<Result<Vec<_>, _>>()?;
 
+        let seed_ranges = seeds
+            .chunks(2)
+            .map(|chunk| SeedRange {
+                start: chunk[0],
+                len: chunk[1],
+            })
+            .collect();
+
         let mut maps = {
             let map_names = [
                 "seed-to-soil",
@@ -98,6 +114,7 @@ impl FromStr for Almanac {
 
         Ok(Self {
             seeds,
+            seed_ranges,
             seed_to_soil: maps.next().unwrap_or_else(|| unreachable!()),
             soil_to_fertilizer: maps.next().unwrap_or_else(|| unreachable!()),
             fertilizer_to_water: maps.next().unwrap_or_else(|| unreachable!()),
@@ -110,18 +127,79 @@ impl FromStr for Almanac {
 }
 
 impl Range {
-    const fn dest(&self, src: u32) -> Option<u32> {
+    const fn map(&self, src: u32) -> Option<u32> {
         if src >= self.start_src && src < self.start_src + self.len {
             Some(self.start_dest + (src - self.start_src))
         } else {
             None
         }
     }
+
+    fn map_range(&self, input: &SeedRange) -> (Option<SeedRange>, Vec<SeedRange>) {
+        let end = self.start_src + self.len;
+        let end_input = input.start + input.len;
+
+        if input.start >= end || end_input <= self.start_src {
+            // No overlap
+            return (None, vec![input.clone()]);
+        }
+
+        let mut unmapped = Vec::new();
+
+        if input.start < self.start_src {
+            // Unmapped ranged before start
+            unmapped.push(SeedRange {
+                start: input.start,
+                len: self.start_src - input.start,
+            });
+        }
+
+        if end_input > end {
+            // Unmapped range after end
+            unmapped.push(SeedRange {
+                start: end,
+                len: end_input - end,
+            });
+        }
+
+        let start_mapped = self.start_src.max(input.start);
+        let end_mapped = end.min(end_input);
+        let mapped = SeedRange {
+            start: self.start_dest + (start_mapped - self.start_src),
+            len: end_mapped - start_mapped,
+        };
+
+        (Some(mapped), unmapped)
+    }
 }
 
 impl Almanac {
-    fn seed_to_location(&self, seed: u32) -> u32 {
-        let mut result = seed;
+    fn map_seeds(&self) -> Vec<u32> {
+        let mut results = Vec::with_capacity(self.seeds.len());
+
+        for &seed in &self.seeds {
+            let mut result = seed;
+
+            for ranges in [
+                &self.seed_to_soil,
+                &self.soil_to_fertilizer,
+                &self.fertilizer_to_water,
+                &self.water_to_light,
+                &self.light_to_temperature,
+                &self.temperature_to_humidity,
+                &self.humidity_to_location,
+            ] {
+                result = Self::map(result, ranges);
+            }
+
+            results.push(result);
+        }
+
+        results
+    }
+
+    fn map_seed_ranges(&self) -> Vec<SeedRange> {
+        let mut seed_ranges = self.seed_ranges.clone();
 
         for ranges in [
             &self.seed_to_soil,
@@ -132,25 +210,52 @@ impl Almanac {
             &self.temperature_to_humidity,
             &self.humidity_to_location,
         ] {
-            result = Self::find_dest(result, ranges);
+            seed_ranges = Self::map_range(seed_ranges, ranges);
         }
 
-        result
+        seed_ranges
     }
 
-    fn find_dest(src: u32, ranges: &[Range]) -> u32 {
+    fn map(src: u32, ranges: &[Range]) -> u32 {
         ranges
             .iter()
-            .find_map(|range| range.dest(src))
+            .find_map(|range| range.map(src))
             .unwrap_or(src)
+    }
+
+    fn map_range(input: Vec<SeedRange>, ranges: &[Range]) -> Vec<SeedRange> {
+        let mut unmapped = input;
+        let mut mapped = Vec::new();
+
+        for range in ranges {
+            let mut unmapped_still = Vec::new();
+
+            for u in unmapped {
+                let (m, u) = range.map_range(&u);
+                if let Some(m) = m {
+                    mapped.push(m);
+                }
+
+                unmapped_still.extend(u);
+            }
+
+            unmapped = unmapped_still;
+        }
+
+        mapped.extend(unmapped);
+        mapped
     }
 }
 
 fn part1(almanac: &Almanac) -> u32 {
+    almanac.map_seeds().into_iter().min().unwrap_or_default()
+}
+
+fn part2(almanac: &Almanac) -> u32 {
     almanac
-        .seeds
-        .iter()
-        .map(|&seed| almanac.seed_to_location(seed))
+        .map_seed_ranges()
+        .into_iter()
+        .map(|range| range.start)
         .min()
         .unwrap_or_default()
 }
@@ -167,6 +272,15 @@ fn main() -> Result<()> {
         assert_eq!(part1, 346_433_842);
     };
 
+    {
+        let start = Instant::now();
+        let part2 = self::part2(&almanac);
+        let elapsed = Instant::now().duration_since(start);
+
+        println!("Part 2: {part2} ({elapsed:?})");
+        assert_eq!(part2, 60_294_664);
+    };
+
     Ok(())
 }
 
@@ -175,21 +289,41 @@ mod tests {
     use super::*;
 
     #[test]
-    fn range_dest() {
+    fn range_map() {
         let range = Range {
             start_src: 98,
             start_dest: 50,
             len: 2,
         };
 
-        assert_eq!(range.dest(97), None);
-        assert_eq!(range.dest(98), Some(50));
-        assert_eq!(range.dest(99), Some(51));
-        assert_eq!(range.dest(100), None);
+        assert_eq!(range.map(97), None);
+        assert_eq!(range.map(98), Some(50));
+        assert_eq!(range.map(99), Some(51));
+        assert_eq!(range.map(100), None);
     }
 
     #[test]
-    fn almanac_find_dest() {
+    fn range_map_range() {
+        let range = Range {
+            start_src: 98,
+            start_dest: 50,
+            len: 2,
+        };
+
+        let (mapped, unmapped) = range.map_range(&SeedRange { start: 97, len: 4 });
+
+        assert_eq!(mapped, Some(SeedRange { start: 50, len: 2 }));
+        assert_eq!(
+            unmapped,
+            [
+                SeedRange { start: 97, len: 1 },
+                SeedRange { start: 100, len: 1 }
+            ]
+        );
+    }
+
+    #[test]
+    fn almanac_map() {
         let ranges = [
             Range {
                 start_src: 98,
@@ -204,15 +338,15 @@ mod tests {
         ];
 
         for src in 0..=49 {
-            assert_eq!(Almanac::find_dest(src, &ranges), src);
+            assert_eq!(Almanac::map(src, &ranges), src);
         }
 
         for src in 50..=97 {
-            assert_eq!(Almanac::find_dest(src, &ranges), src + 2);
+            assert_eq!(Almanac::map(src, &ranges), src + 2);
         }
 
-        assert_eq!(Almanac::find_dest(98, &ranges), 50);
-        assert_eq!(Almanac::find_dest(99, &ranges), 51);
-        assert_eq!(Almanac::find_dest(100, &ranges), 100);
+        assert_eq!(Almanac::map(98, &ranges), 50);
+        assert_eq!(Almanac::map(99, &ranges), 51);
+        assert_eq!(Almanac::map(100, &ranges), 100);
     }
 }
