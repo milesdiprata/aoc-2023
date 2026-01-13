67ce644413b1cbc22ad045eb53f8dea0bc46d55b
Miles di Prata <milesdiprata3@gmail.com>
2026-01-13 08:47:57 -0800

    Day 9 part 2

--- src/day9.rs
+++ src/day9.rs
@@ -43,10 +43,33 @@ impl History {
 
         lasts.into_iter().rev().sum()
     }
+
+    fn extrapolate_rev(&self) -> i32 {
+        let mut sequence = self.vals.clone();
+        let mut firsts = vec![];
+
+        while sequence.iter().any(|&val| val != 0) {
+            firsts.push(sequence.first().copied().unwrap());
+
+            let mut next = vec![];
+
+            for window in sequence.windows(2) {
+                next.push(window[1] - window[0]);
+            }
+
+            sequence = next;
+        }
+
+        firsts.into_iter().rev().fold(0, |acc, val| val - acc)
+    }
 }
 
 fn part1(histories: &[History]) -> i32 {
     histories.iter().map(History::extrapolate).sum()
+}
+
+fn part2(histories: &[History]) -> i32 {
+    histories.iter().map(History::extrapolate_rev).sum()
 }
 
 fn main() -> Result<()> {
@@ -62,6 +85,15 @@ fn main() -> Result<()> {
 
         println!("Part 1: {part1} ({elapsed:?})");
         assert_eq!(part1, 1_708_206_096);
+    };
+
+    {
+        let start = Instant::now();
+        let part2 = self::part2(&histories);
+        let elapsed = Instant::now().duration_since(start);
+
+        println!("Part 2: {part2} ({elapsed:?})");
+        assert_eq!(part2, 1_050);
     };
 
     Ok(())
